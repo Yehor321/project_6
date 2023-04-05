57eacfff04392145f3baa02e3ee2ab7238851506
Yehor <Yehor321@gmail.com>
2023-04-05 14:00:02 +0300

    .

--- js/sign_in.js
+++ js/sign_in.js
@@ -6,7 +6,24 @@ const name = document.getElementsByName("name")[0];
 const surname = document.getElementsByName("surname")[0];
 
 form.onsubmit = (e) => {
+
     e.preventDefault();
+
+    if (name.value.length < 4) {
+        alert("Будь ласка введіть довше ім'я");
+        return;
+    }
+    
+    if (surname.value.length < 4) {
+        alert("Будь ласка введіть довше прізвище");
+        return;
+    }
+    
+    if (password.value.length < 8) {
+        alert("Будь ласка введіть правильний пароль довжина якого має бути 8, або більше");
+        return;
+    }
+
     alert( name.value + " " + surname.value + ", раді тебе бачити!");
     console.log(email.value, password.value);
 }
